9c3c54fb6a90f5fba1f3b70184ff9887a2c04deb
Vanja Benjocki <benjocki.vanja@gmail.com>
2019-06-12 17:13:21 +0200

    Add visual studio code in dock

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -101,6 +101,7 @@ defaults write com.apple.dock persistent-apps -array ''
 
 permament_dock "/Applications/Google Chrome.app/"
 permament_dock "/Applications/Hyper.app/"
+permament_dock "/Applications/Visual Studio Code.app/"
 
 # Disable Dashboard
 defaults write com.apple.dashboard mcx-disabled -bool true
