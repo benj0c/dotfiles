1124f89b1d88031c50d3c89c6603a818c4077fe3
Vanja Benjocki <benjocki.vanja@gmail.com>
2021-08-28 13:48:14 +0200

    Update docker aliases to use docker compose v2 cli commands

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,3 +1,4 @@
-alias dcu="docker-compose up"
-alias dcd="docker-compose down"
-alias dcp="docker-compose ps"
+alias dcu="docker compose up"
+alias dcd="docker compose down"
+alias dcp="docker compose ps"
+alias dcl="docker compose logs -f"
